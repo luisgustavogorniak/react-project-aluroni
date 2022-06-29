3ab0b3aa4e635d2edc6cbc9a087d507df99f1d15
LUIS GUSTAVO GORNIAK <gustavogorniak@gmail.com>
2022-06-29 16:45:27 -0300

    creating routes dom hooks

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import styles from './Buscador.module.scss'
+import React from 'react';
+import styles from './Buscador.module.scss';
 import { CgSearch } from 'react-icons/cg';
 
 interface Props {
@@ -8,15 +8,15 @@ interface Props {
 }
 
 export default function Buscador({ busca, setBusca }: Props) {
-    return <div className={styles.buscador}>
-        <input
-            value={busca}
-            onChange={(evento) => setBusca(evento.target.value)}
-            placeholder="Buscar"
-        />
-        <CgSearch 
-            size={20}
-            color="#4C4D5E"
-        />
-    </div>
+  return <div className={styles.buscador}>
+    <input
+      value={busca}
+      onChange={(evento) => setBusca(evento.target.value)}
+      placeholder="Buscar"
+    />
+    <CgSearch 
+      size={20}
+      color="#4C4D5E"
+    />
+  </div>;
 }
--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,34 +1,25 @@
 
-import styles from './Item.module.scss'
-import cardapio from '../itens.json'
-import classNames from 'classnames';
+import styles from './Item.module.scss';
+import { Prato } from 'types/Prato';
+import TagsPrato from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
 
-type Props = typeof cardapio[0];
 
-export default function Item(props: Props) {
-    const { title, description, category, id, photo, price, serving, size } = props;
-    return (
-        <div className={styles.item}>
-            <div className={styles.item__imagem}>
-               <img src={photo} alt={title} /> 
-            </div>
-            <div className={styles.item__descricao}>
-                <div>
-                    <h2> {title} </h2>
-                    <p> {description} </p>
-                </div>
-                <div className={styles.item__tags}>
-                <div className={classNames(
-                    styles.item__tipo,
-                    styles[`item__tipo__${category.label.toLowerCase()}`]
-                )}>
-                    {category.label}
-                </div>
-                    <div className={styles.item__porcao}>{size}</div>
-                    <div className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : "s"}</div>
-                    <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
-                </div>
-            </div>
+export default function Item(props: Prato) {
+  const { id, title, description, photo } = props;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} /> 
+      </div>
+      <div className={styles.item__descricao}>
+        <div>
+          <h2> {title} </h2>
+          <p> {description} </p>
         </div>
-    )
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
 }
